5a6f2c2cce6388d2750aab364c5008585395e8b2
Čedomir Stevanović <cedomirstevanovic8@gmail.com>
2023-10-30 13:39:05 +0100

    Initial commit

---
+++ equipment_booker/src/test/java/com/example/equipment_booker/EquipmentBookerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.equipment_booker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentBookerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
